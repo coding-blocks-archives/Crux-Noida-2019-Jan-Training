f1d12d6ebd49909191d1977d4165d3077684ab24
Keenarrior <anujgargcse@gmail.com>
2019-03-07 14:25:53 +0530

    bst

--- src/com/codingblocks/lecture_24/BinarySearchTreeBalancing.java
+++ src/com/codingblocks/lecture_24/BinarySearchTreeBalancing.java
@@ -20,10 +20,79 @@ public class BinarySearchTreeBalancing {
             node.right = add(node.right, value);
         }
 
-        // update heights
         node.height = Math.max(height(node.left), height(node.right)) + 1;
 
+        // balancing
+        node = balancing(node);
+
         return node;
+    }
+
+    private Node balancing(Node node) {
+        if(height(node.left) - height(node.right) > 1){
+            if (height(node.left.left) - height(node.left.right) < 0) {
+                node.left = leftRotate(node.left);
+            }
+            return rightRotate(node);
+        }
+
+        if(height(node.left) - height(node.right) < -1){
+            if (height(node.right.left) - height(node.right.right) > 0) {
+                node.right = rightRotate(node.right);
+            }
+            return leftRotate(node);
+        }
+
+        return node;
+    }
+
+    public Node rightRotate(Node x){
+        Node  y = x.left;
+        Node temp = y.right;
+
+        y.right = x;
+        x.left = temp;
+
+        x.height = Math.max(height(x.left), height(x.right)) + 1;
+        y.height = Math.max(height(y.left), height(y.right)) + 1;
+
+        return y;
+    }
+
+    public Node leftRotate(Node y){
+        Node x = y.right;
+        Node temp = x.left;
+
+        x.left = y;
+        y.right = temp;
+
+        y.height = Math.max(height(y.left), height(y.right)) + 1;
+        x.height = Math.max(height(x.left), height(x.right)) + 1;
+
+        return x;
+    }
+
+
+    public boolean isBalanced(){
+        return isBalanced(this.root);
+    }
+
+    private boolean isBalanced(Node node) {
+        if(node == null){
+            return true;
+        }
+
+        int diff = Math.abs(height(node.left) - height(node.right));
+
+        if (diff > 1){
+            return false;
+        }
+
+        return isBalanced(node.left) && isBalanced(node.right);
+    }
+
+    public int height(){
+        return height(root);
     }
 
     private int height(Node node){
@@ -33,6 +102,8 @@ public class BinarySearchTreeBalancing {
 
         return node.height;
     }
+
+
 
     public void generateFromSorted(int[] nums){
         this.root = generateFromSorted(nums, 0, nums.length - 1);
--- src/com/codingblocks/lecture_24/TreeClient.java
+++ src/com/codingblocks/lecture_24/TreeClient.java
@@ -2,15 +2,23 @@ package com.codingblocks.lecture_24;
 
 public class TreeClient {
     public static void main(String[] args) {
-        BinarySearchTree tree = new BinarySearchTree();
+        BinarySearchTreeBalancing tree = new BinarySearchTreeBalancing();
 
-        int[] nums = new int[1000000];
+//        int[] nums = new int[1000000];
 
-        for (int i = 0; i < nums.length; i++) {
-            nums[i] = i;
+//        for (int i = 0; i < nums.length; i++) {
+//            nums[i] = i;
+//        }
+
+//        tree.generateFromSorted(nums);
+
+        for (int i = 0; i < 40; i++) {
+            tree.add(i);
         }
 
-        tree.generateFromSorted(nums);
+        tree.inOrder();
+
+        System.out.println(tree.height());
 
 
 //        tree.display();
